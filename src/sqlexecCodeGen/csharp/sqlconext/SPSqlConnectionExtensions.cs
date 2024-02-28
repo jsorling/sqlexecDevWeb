28423b9abcdb372729f7deb7a4c916b2c8bed1bc
Jörgen Sörling <jorgen@sorling.eu>
2024-02-28 23:15:09 +0100

    sqlexeccodegen

---
+++ src/sqlexecCodeGen/csharp/sqlconext/SPSqlConnectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Sorling.SqlExecMeta.objects.storedprocedures;
+using Sorling.SqlExecMeta.sql2code.csharp;
+using System.Text;
+
+namespace Sorling.sqlexecCodeGen.csharp.sqlconext;
+
+public static class SPSqlConnectionExtensions
+{
+   public static string GetCSharpConnectionStyleCode(this IEnumerable<SqlStoredProcedureDefCmd.Result> sp) {
+      Sql2CSharp csmapper = new();
+      return new StringBuilder().Append("using System.Data.SqlClient;\n\nnamespace ns;\n\n")
+         .Append($"public static class {csmapper.MapSql2ProperName(sp.First().DBName)}SqlConnectionExtension\n {{\n")
+         .Append("  public static SqlOperations DBName(this SqlConnection sqlConnection) => new(sqlConnection);\n")
+         .Append("}\n\npublic partial class SqlOperations\n{  \n")
+         .Append("}\n").ToString();
+   }
+}
