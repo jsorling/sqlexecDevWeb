3445597c06fe8852d589331f9452818fa861a6a9
jsorling <jorgen@sorling.eu>
2025-04-27 05:56:14 +0200

    new assemblyname, libman

--- src/sqlexecDevWeb/pages/sqlsrv/Index.cshtml.cs
+++ src/sqlexecDevWeb/pages/sqlsrv/Index.cshtml.cs
@@ -7,7 +7,7 @@ namespace Sorling.sqlexecDevWeb.pages.sqlsrv;
 
 public class IndexModel : PageModel
 {
-   public IEnumerable<SqlConnectionHelper.ListDBCmd.ListDBRes>? DBs { get; private set; }
+   public IEnumerable<SqlConnectionHelper.ListDBCmd.ListDBRes> DBs { get; private set; } = [];
 
    public async Task OnGetAsync([FromServices] ISqlAuthService sqlConAuth) => DBs = await sqlConAuth.GetDBsAsync();
 
